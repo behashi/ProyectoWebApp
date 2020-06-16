db6a460c3f7cdd3648d9f70443772b7635162dbe
elgrau96 <graulex71@gmail.com>
2020-06-16 13:28:14 +0200

    Proyecto acabado 100%

---
+++ src/servlets/ModificarDniAl.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ModificarDniAl
+ */
+public class ModificarDniAl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModificarDniAl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session=request.getSession(false);
+		String dni = request.getParameter("Alumno").toString();
+		
+		session.setAttribute("dniAl",dni);
+		RequestDispatcher rd = request.getRequestDispatcher("/DatosAlumno.html");
+	    rd.forward(request, response);
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
